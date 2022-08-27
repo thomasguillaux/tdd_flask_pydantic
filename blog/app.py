34aa5b58d976621aa14b73cb2ea3d2dfb8aa062a
thomasguillaux <thomasguillaux@gmail.com>
2022-08-27 14:53:46 +0200

    test: completing e2e tests

--- blog/app.py
+++ blog/app.py
@@ -1,6 +1,7 @@
 from crypt import methods
 
 from flask import Flask, jsonify, request
+from pydantic import ValidationError
 
 from blog.commands import CreateArticleCommand
 from blog.queries import GetArticleByIDQuery, ListArticlesQuery
@@ -27,6 +28,12 @@ def list_articles():
     query = ListArticlesQuery()
     records = [record.dict() for record in query.execute()]
     return jsonify(records)
+
+@app.errorhandler(ValidationError)
+def handle_validation_excpetion(error):
+    response = jsonify(error.errors())
+    response.status_code = 400
+    return response
 
 if __name__ == "main":
     app.run()
--- tests/test_article/test_app.py
+++ tests/test_article/test_app.py
@@ -2,6 +2,7 @@ import json
 import pathlib
 
 import pytest
+import requests
 from blog.app import app
 from blog.models import Article
 from jsonschema import RefResolver, validate
@@ -91,3 +92,62 @@ def test_list_articles(client):
     )
 
     validate_payload(response.json, "ArticleList.json")
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        {
+            "author": "John Doe",
+            "title": "New Article",
+            "content": "Some extra awesome content"
+        },
+        {
+            "author": "John Doe",
+            "title": "New Article",
+        },
+        {
+            "author": "John Doe",
+            "title": None,
+            "content": "Some extra awesome content"
+        }
+    ]
+)
+def test_create_article_bad_request(client, data):
+    """
+    GIVEN request data with invalid values or missing attributes
+    WHEN endpoint /create-article/ is called
+    THEN it should return status 400
+    """
+    response = client.post(
+        "/create-article/",
+        data=json.dumps(
+            data
+        ),
+        content_type="application/json"
+    )
+
+    assert response.status_code == 400
+    assert response.json is not None
+
+@pytest.mark.e2e
+def test_create_list_get(client):
+    requests.post(
+         "http://127.0.0.1:5000/create-article/",
+        json={
+            "author": "john@doe.com",
+            "title": "New Article",
+            "content": "Some extra awesome content"
+        }
+    )
+
+    response = requests.get(
+        "http://127.0.0.1:5000/article-list/",
+    )
+
+    articles = response.json()
+
+    response = requests.get(
+        f"http://127.0.0.1:5000/article/{articles[0]['id']}/",
+    )
+
+    assert response.status_code == 200
